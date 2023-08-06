31eb0c23f9e8afc434ce020e9e835b57ce1b272f
Shohanur Rahman <shohanur.swe@gmail.com>
2023-08-06 23:21:20 +0600

    add new tC

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,11 +1,8 @@
-describe('Google Homepage Test', () => {
-    it('should navigate to Google', () => {
-      // Navigate to Google
-      cy.visit('https://www.google.com/');
-      debugger;
-      // Assert that the title contains "Google"
-      cy.title().should('contain', 'Google');
-      cy.get('#APjFqb').type("wow man")
-      cy.get('#APjFqb').type('{enter}')
-    });
-  })
+describe('Dsp Testing', ()=>{
+    it("Login Functionality ", ()=>{
+        cy.visit('https://dsp-stage.eskimi.com/')
+        cy.get('#username').type('###')
+        cy.get('#password').type('###')
+        cy.get('input[type="submit"]').click()
+    })
+})
--- cypress/integration/examples/test2.js
+++ cypress/integration/examples/test2.js
@@ -1,9 +1,17 @@
 describe('Home page', ()=>{
     it('search item and check select' , ()=>{
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.visit('http://rahulshettyacademy.com/seleniumPractise/#/')
         cy.get('.search-keyword').type('ca')
-        cy.get('.product:visible').should('have.length', 4)
+        cy.get('.products').find('.product').should('have.length', 4)
         cy.get('.search-keyword').should('have.value', 'ca')
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('.products').find('.product').each(($el)=>{
+        let textveg= $el.find('h4.product-name').text()
+            if(textveg.includes('Cashews')){
+               cy.wrap($el).find('button').click()
+            }
 
+
+        })
     })
 })
