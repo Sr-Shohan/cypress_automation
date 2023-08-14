4a3b1be059463809060945caeff63381a6f92bc3
Shohanur Rahman <shohanur@eskimi.com>
2023-08-14 23:59:43 +0600

    update TCs

--- cypress/integration/examples/test03.js
+++ cypress/integration/examples/test03.js
@@ -1,6 +1,7 @@
 describe('Browser ACtions', ()=>{
     it("should load correct url", ()=>{
         cy.visit('https://dsp.eskimi.com')
+        cy.url().should('include', 'eskimi.com')
 
     })
 })
