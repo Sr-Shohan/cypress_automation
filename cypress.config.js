b22be3edda8bd35ec8af10d1ac9778b608ea0c4c
Shohanur Rahman <shohanur@eskimi.com>
2023-08-14 23:18:30 +0600

    added new viewport

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/examples/*.js',
+    viewportWidth: 1980,
+    viewportHeight: 1080
   },
 });
---
+++ cypress/integration/examples/test03.js
@@ -0,0 +1,6 @@
+describe('Browser ACtions', ()=>{
+    it("should load correct url", ()=>{
+        cy.visit('https://dsp.eskimi.com')
+
+    })
+})
